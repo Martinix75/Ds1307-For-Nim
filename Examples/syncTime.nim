2b2d6a8200793c6347580bb26004ad56233f8e2a
Martin Andrea <65339251+Martinix75@users.noreply.github.com>
2025-05-30 10:57:17 +0200

    SyncTime Ver 0.1.1
    - Translated into english.
    - correct reading writing on serial.

--- Examples/syncTime.nim
+++ Examples/syncTime.nim
@@ -1,17 +1,18 @@
-import std/[times, strutils]
+import std/[times, strutils,os]
 from strformat import fmt
 import serial
 
 let 
-  syncTimeVer = "0.1.0"
+  syncTimeVer = "0.1.1"
   #usb = newSerialPort("/dev/ttyACM0") #per ora creiamo la connessine fissa (non si puo scegliere.
 
 proc scrivi(s: string) =
   let usb = newSerialPort("/dev/ttyACM0") #per ora creiamo la connessine fissa (non si puo scegliere.
   var str = newString(1024)
-  usb.open(19200, Parity.None, 8, StopBits.One, readTimeout=1000)#setta i parametri per la comunicazione seriale usb.
+  usb.open(19200, Parity.None, 8, StopBits.One, readTimeout=10)#setta i parametri per la comunicazione seriale usb.
   discard usb.write(s)
-  let ppp =  (usb.read(str)) #deve essereci non so perchè forse aspetta una risposta (anche se non ce).
+  sleep(100)
+  #let ppp =  (usb.read(str)) #deve essereci non so perchè forse aspetta una risposta (anche se non ce).
   usb.close()
 
 
@@ -19,19 +20,19 @@ echo("---------------------------")
 echo(fmt"   syncTime Ver: {syncTimeVer}    ")
 echo("---------------------------")
 echo()
-stdout.write("Vuoi Sincronizzare il dispositivo al Pc Ora? (s/n): ")
+stdout.write("Do you want to synchronize your watch with that of your PC? (y/n): ")
 let reponse = stdin.readline().toLowerAscii()
-if reponse == "s":
+if reponse == "y":
   let 
     time = getClockStr()
     data = getDateStr()
     spdata = data.split("-")
     dywe = getDayOfWeek(parseInt(spdata[2]), Month(parseInt(spdata[1])) ,parseInt(spdata[0])).ord()
     dato: string = "#" & time & "#" & data & "#" & $dywe & "#"
-  echo(fmt"Stringa ottenuta --> {dato}")
+  echo(fmt"String Obtained --> {dato}")
   scrivi(dato)
   
 elif reponse == "n":
   echo("Bye!")
 else:
-  echo("Comando errato!!!!")
+  echo("Incorrect Command!!!!")
